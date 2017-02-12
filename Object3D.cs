a55096431d3bcafb68737a808da192e8c55ef459
Kirby <kirby@verbowski.com>
2017-02-11 16:17:47 -0800

    Add files via upload

--- Object3D.cs
+++ Object3D.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
 
 /*
     Object3D.cs
@@ -71,11 +74,17 @@ namespace Graphics3D {
             }
         }
 
+        protected Matrix4x4 _transformMatrix = Matrix4x4.identity;
+        public Matrix4x4 transformMatrix {
+            get { return _transformMatrix; }
+            set { _transformMatrix = value; TransformUpdate(); }
+        }
+
         /// <summary>
         /// Returns a unit vector which points in this object's local forward direction
         /// </summary>
         public Vector3 forward {
-            get { return Vector4.ToVector3(Matrix4x4.CombinedRotation(this.rotation) * new Vector4(0, 0, 1, 1)).normalized; }
+            get { return (Matrix4x4.CombinedRotation(-this.rotation).GetRow(2).ToVector3()); }
             private set { }
         }
 
@@ -83,7 +92,7 @@ namespace Graphics3D {
         /// Returns a unit vector which points in this object's local up direction
         /// </summary>
         public Vector3 up {
-            get { return Vector4.ToVector3(Matrix4x4.CombinedRotation(this.rotation) * new Vector4(0, 1, 0, 1)).normalized; }
+            get { return (Matrix4x4.CombinedRotation(-this.rotation).GetRow(1)).ToVector3(); }
             private set { }
         }
 
@@ -91,7 +100,7 @@ namespace Graphics3D {
         /// Returns a unit vector which points in this object's local right direction
         /// </summary>
         public Vector3 right {
-            get { return Vector4.ToVector3(Matrix4x4.CombinedRotation(this.rotation) * new Vector4(1, 0, 0, 1)).normalized; }
+            get { return (Matrix4x4.CombinedRotation(-this.rotation).GetRow(0).ToVector3()); }
             private set { }
         }
 
@@ -99,6 +108,18 @@ namespace Graphics3D {
         /// This method will be called when this transform's location, rotation, or scale is changed
         /// </summary>
         public TransformUpdateDelegate TransformUpdate;
+        #endregion
+
+        #region Member Methods
+
+        /// <summary>
+        /// Rotates this transform so that forward points at target
+        /// </summary>
+        /// <param name="target">A direction vector NOT a position vector</param>
+        public void LookAt(Vector3 target) {
+            rotation = Matrix4x4.FromToRotation(Vector3.unitVectorZ, target).GetEulerAngles();
+        }
+
         #endregion
 
         #region Constructors
@@ -111,6 +132,7 @@ namespace Graphics3D {
             this._position = position;
             this._rotation = rotation;
             this._scale = scale;
+            
         }
         #endregion
     }
@@ -142,6 +164,8 @@ namespace Graphics3D {
         /// </summary>
         public bool selected = false;
 
+        private Mesh mesh;
+
 
         public GetEdgeDelegate getEdgeDelegate;
         #endregion
@@ -152,7 +176,7 @@ namespace Graphics3D {
                 return;
             }
             for (int i = 0; i < vertices.Length; i++) {
-                vertices[i] = Vector4.ToVector3(Matrix4x4.CombinedRotation(transform.rotation) * Vector3.ToVector4(new Vector3(unitVertices[i].x * transform.scale.x, unitVertices[i].y * transform.scale.y, unitVertices[i].z * transform.scale.z)));
+                vertices[i] = (Matrix4x4.CombinedRotation(transform.rotation) * (new Vector4(unitVertices[i].x * transform.scale.x, unitVertices[i].y * transform.scale.y, unitVertices[i].z * transform.scale.z, 1))).ToVector3();
                 vertices[i] += this.transform.position;
             }
         }
@@ -186,12 +210,116 @@ namespace Graphics3D {
                 
             }
         }
+
+        private Mesh ReadObjFile(StreamReader stream) {
+            string line;
+            string[] words;
+            char[] lineCh;
+            List<Vector3> vertices = new List<Vector3>();
+            Vector3 temp = Vector3.zero;
+            List<List<int>> faces = new List<List<int>>();
+            List<int[]> edges = new List<int[]>();
+            int face = 0;
+            Mesh result = new Mesh();
+
+
+            while ((line = stream.ReadLine()) != null) {
+                lineCh = line.ToCharArray();
+                if (lineCh[0] == '#') {
+                    continue;
+                }
+
+                words = line.Split(' ');
+
+                switch (words[0]) {
+                    case "v":
+                        for (int i = 1; i < 4; i++) {
+                            temp[i - 1] = float.Parse(words[i]);
+                        }
+                        vertices.Add(temp);
+                        break;
+
+                    case "f":
+                        StringBuilder sb = new StringBuilder();
+                        int j = 0;
+
+                        for (int i = 1; i < words.Length; i++) {
+                            while (words[i].ElementAt(j) != '/') {
+                                sb.Append(words[i].ElementAt<char>(j));
+                                j++;
+                            }
+                            j = 0;
+                            faces.Add(new List<int>());
+                            faces[face].Add(int.Parse(sb.ToString()) - 1);
+                            sb.Clear();
+                        }
+                        face++;
+                        break;
+
+                    case "o":
+                        result.name = words[1];
+                        break;
+                }
+
+            }
+            stream.Close();
+
+            result.vertices = vertices.ToArray();
+            result.faces = new int[faces.Count][];
+            for (int i = 0; i < faces.Count; i++) {
+                result.faces[i] = faces[i].ToArray();
+            }
+
+            int current = 0;
+            for (int i = 0; i < result.faces.Length; i++) {
+                for (int j = 0; j < result.faces[i].Length - 1; j++) {
+
+                    edges.Add(new int[2]);
+                    edges[current][0] = result.faces[i][j];
+                    edges[current][1] = result.faces[i][j + 1];
+                    current++;
+                }
+                if (result.faces[i].Length != 0) {
+
+                    edges.Add(new int[2]);
+                    edges[current][0] = result.faces[i][0];
+                    edges[current][1] = result.faces[i][faces[i].Count - 1];
+                    current++;
+                }
+            }
+
+            result.edges = edges.ToArray();
+
+            return result;
+        }
+
+        private Tuple<Vector3, Vector3>[] EdgesFromMesh(Mesh mesh) {
+            List<Tuple<Vector3, Vector3>> result = new List<Tuple<Vector3, Vector3>>();
+            for (int i = 0; i < mesh.edges.Length; i++) {
+                result.Add(Tuple.Create<Vector3, Vector3>(this.vertices[mesh.edges[i][0]], this.vertices[mesh.edges[i][1]]));
+            }
+            return result.ToArray();
+        }
         #endregion
+
 
         #region Constructors
         public Object3D(Vector3 position, Vector3 rotation, Vector3 scale) {
             getEdgeDelegate = new GetEdgeDelegate(DefaultGetEdge);
             this.transform = new Transform(position, rotation, scale, TransformUpdate);
+        }
+
+        /// <summary>
+        /// Create a new Object3D from an .obj file at the given path
+        /// </summary>
+        public Object3D(StreamReader sr) {
+            if(sr != null) {
+                this.mesh = ReadObjFile(sr);
+            }
+            this.vertices = mesh.vertices;
+            this.unitVertices = mesh.vertices;
+            transform = new Transform(Vector3.zero, Vector3.zero, Vector3.one, TransformUpdate);
+            getEdgeDelegate = new GetEdgeDelegate(() => EdgesFromMesh(this.mesh));
         }
         #endregion
     }
